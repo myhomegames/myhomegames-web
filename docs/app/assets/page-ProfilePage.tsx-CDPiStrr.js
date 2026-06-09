9ef1618417ace5bb1e7d984eb4ffcd62e351f6f2
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-06-09 12:10:29 +0200

    new release

---
+++ docs/app/assets/page-ProfilePage.tsx-CDPiStrr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./react-vendor-Cdfluhav.js";import{P as o}from"./page-HomePageClassic.tsx-BDyKd1B5.js";function a(){return r.jsx(o,{variant:"page"})}export{a as P};
--- docs/app/assets/page-ProfilePage.tsx-DNVjOA7B.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./react-vendor-Cdfluhav.js";import{P as o}from"./page-HomePageClassic.tsx-2v0nkX7a.js";function a(){return r.jsx(o,{variant:"page"})}export{a as P};
