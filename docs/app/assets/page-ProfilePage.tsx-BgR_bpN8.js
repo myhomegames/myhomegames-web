ef93208ff751988316534610c13f5347c75bc1f9
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-07-19 13:15:04 +0200

    Streaming

--- docs/app/assets/page-ProfilePage.tsx-BgR_bpN8.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./react-vendor-BzXkVCRW.js";import{a4 as o}from"./page-CatalogGameDetailPage.tsx-RbN0Je2N.js";function e(){return r.jsx(o,{variant:"page"})}export{e as P};
---
+++ docs/app/assets/page-ProfilePage.tsx-XyQMFISj.js
@@ -0,0 +1,1 @@
+import{j as r}from"./react-vendor-BzXkVCRW.js";import{a4 as o}from"./page-CatalogGameDetailPage.tsx-B1Y1eMjl.js";function e(){return r.jsx(o,{variant:"page"})}export{e as P};
