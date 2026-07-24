379923424c93aad876d913c4b5c1899fbb280014
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-07-24 10:50:18 +0200

    ps3 skin

--- docs/app/assets/page-ProfilePage.tsx-8TcQOKx0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./react-vendor-KJOBGNDP.js";import{P as o}from"./page-HomePageClassic.tsx-BlSIc_9n.js";function a(){return r.jsx(o,{variant:"page"})}export{a as P};
---
+++ docs/app/assets/page-ProfilePage.tsx-C-dJaOrx.js
@@ -0,0 +1,1 @@
+import{j as r}from"./react-vendor-KJOBGNDP.js";import{P as o}from"./page-HomePageClassic.tsx-kcQqnUpk.js";function a(){return r.jsx(o,{variant:"page"})}export{a as P};
