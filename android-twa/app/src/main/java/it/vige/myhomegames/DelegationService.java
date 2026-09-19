ba650fab8f057050208ecac4e232a73266f6b5cb
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-09-19 09:31:22 +0200

    New release

---
+++ android-twa/app/src/main/java/it/vige/myhomegames/DelegationService.java
@@ -0,0 +1,14 @@
+package it.vige.myhomegames;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
